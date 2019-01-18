dbd986e997ef1a7f1523717a6933630530e236dc
Orrett Coke <orrettcoke@aol.com>
2019-01-18 15:31:31 -0500

    Created Login.vue

---
+++ front-end/src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/Salu-Pro-Logo.gif" width="150px">
+    <div id="Input Boxes">
+        <input value="Username" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
