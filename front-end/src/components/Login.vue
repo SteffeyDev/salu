e929ddb68facf221cb66a369696317724ab4516a
Orrett Coke <orrettcoke@aol.com>
2019-01-21 02:17:30 -0500

    Update Login.vue

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -4,19 +4,26 @@
     <div class="container">
       <form>
         <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email">
+          <div class="row">
+            <div class="col-0 vcenter"><label for="username">Username</label></div>
+            <div class="col"><input type="text" class="form-control" id="username1" aria-describedby="usernameHelp" placeholder="Enter your username"></div>
+          </div>
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="col-0"><label for="exampleInputPassword1">Password</label></div>
+              <div class="col"><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></div>
+            </div>
+          </div>
         <div class="form-group form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <label class="form-check-label" for="confirmationCheckbox">Check me out</label>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group ">
+          <button type="submit" class="btn btn-primary">Login</button>
+          <button type="submit" class="btn btn-primary">Sign Up</button>
+        </div>
       </form>
     </div>
   </div>
@@ -39,5 +46,11 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .vcenter {
+    display: inline-block;
+    vertical-align: middle;
+    float: none;
   }
 </style>
