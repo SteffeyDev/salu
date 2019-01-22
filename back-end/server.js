bace02f0f24d481dda0c772eccb14156dcfdce86
SteffeyDev <steffeydev@icloud.com>
2019-01-22 18:15:22 -0500

    Moving config to one file, more formatting fix

--- back-end/server.js
+++ back-end/server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const db = require('./config/db');
-
+const config = require('./config');
 
 const app = express();
 
-const port = 80;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url,  { useNewUrlParser: true }, (err, database) => {
-	if (err) return console.log(err)
-		
-	const dbase = database.db("Contacts")
-	require('./app/routes')(app, dbase);
+MongoClient.connect(config.MONGO_URI,  { useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(err)
+    
+  require('./app/routes')(app, database);
 
-	app.listen(port, () => {
-	console.log('We are live on ' + port);
-	});
+  app.listen(config.LISTEN_PORT, () => {
+    console.log('The API is live on ' + config.LISTEN_PORT);
+  });
 });
