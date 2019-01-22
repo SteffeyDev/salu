8227f64ae77acb10263f70af33d58caeda7c45ff
Raheim Scantlebury <kiouseh@yahoo.com>
2019-01-22 17:27:17 -0500

    API Test
    Initial check of API - subject to change in future

---
+++ back-end/app/routes/index.js
@@ -0,0 +1,6 @@
+const contactRoutes = require('./contact_routes');
+
+module.exports = function(app, db){
+	contactRoutes(app, db);
+	//other routes groups go here in the future
+};
---
+++ back-end/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url : 'mongodb://raheim:raheim123@ds161104.mlab.com:61104/practice'
+}
