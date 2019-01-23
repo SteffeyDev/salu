c8a4699a8d9f6769f393f4d87b61dbc043953641
SteffeyDev <steffeydev@icloud.com>
2019-01-23 10:34:57 -0500

    Switching server to mongoose

--- back-end/server.js
+++ back-end/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const config = require('./config');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-MongoClient.connect(config.MONGO_URI,  { useNewUrlParser: true }, (err, database) => {
-  if (err) return console.log(err)
-    
-  require('./app/routes')(app, database);
+const db = mongoose.connect(config.MONGO_URI, { useNewUrlParser: true });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  require('./app/routes')(app, db);
 
   app.listen(config.LISTEN_PORT, () => {
     console.log('The API is live on ' + config.LISTEN_PORT);
