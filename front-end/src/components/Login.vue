969141f882cce1137d470c1d2867358de3676d4a
SteffeyDev <steffeydev@icloud.com>
2019-01-24 17:06:53 -0500

    removing app style from login component

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -101,7 +101,6 @@
 </template>
 
 <script>
-
   export default {
     name: 'login',
     data: () => ({
@@ -113,12 +112,4 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
