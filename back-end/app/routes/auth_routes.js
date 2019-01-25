f13c4272307d39ea7faef883f243d558c35d098c
SteffeyDev <steffeydev@icloud.com>
2019-01-25 15:34:49 -0500

    Structure for auth routes

--- back-end/app/routes/auth_routes.js
+++ back-end/app/routes/auth_routes.js
@@ -1,21 +1,45 @@
-const ObjectID = require('mongodb').ObjectID;
 const User = require('../../models/User.js');
 const contactSchema = require('../../schemas/ContactSchema.js');
-const email = 'raheim@raheim.com'
 
-module.exports = function(app, dbase) {
+module.exports = function(app) {
 
   //Creates User
   app.post('/auth/create', (req,res) => {
-    let user = new User({ email: email, password: 'raheim123'});
-		user.save(function (err, user) {
-			if (err) {
-				res.send(err);
-			} else {
-				res.send('Success!');
-			}
-		});
+    let user = new User({ email: req.body.email, password: req.body.password });
+    user.save(function (err, user) {
+      if (err) {
+        res.send(err);
+      } else {
+        // Set cookie with JWT token
+
+        res.send(user);
+      }
+    });
   });
 
+  //Login User
+  app.post('/auth/login', (req,res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (err || !user) {
+        res.send({error: 'Username or password incorrect'});
+      } else {
+        user.comparePassword(req.body.password, (err, success) => {
+          if (success) {
+            // Set cookie with JWT token
+
+            res.status(200).send();
+          } else {
+            res.status(401).send({error: 'Username or password incorrect'})
+          }
+        });
+      }
+    });
+  });
+
+  app.post('/auth/logout', (req,res) => {
+    // Remove JWT from cookie
+
+    res.status(200).send()
+  });
 
 };
