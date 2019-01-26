49d5e2200bb06f6bb161dafb4dd1fb903ee56fe3
Michael Ibeh <mibeh18@gmail.com>
2019-01-26 17:56:51 -0500

    Create jwt.js
    initial jwt strategy

---
+++ back-end/strategies/jwt.js
@@ -0,0 +1,21 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'SaluContactManager!@#$%'; //normally store this in process.env.secret
+
+module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
+  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+};
