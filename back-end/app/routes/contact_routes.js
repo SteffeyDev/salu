d0712a2f0162cb38a1945f80de0e6054cad7777b
BoogalooBill <46578998+BoogalooBill@users.noreply.github.com>
2019-01-26 19:56:18 -0500

    Updated PUT and DELETE routes in contact_routes

--- back-end/app/routes/contact_routes.js
+++ back-end/app/routes/contact_routes.js
@@ -30,18 +30,37 @@ module.exports = function(app, dbase) {
 
   //Deletes contacts by ID
   app.delete('/contacts/:id', (req,res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
+    User.findOne({ email: email }, (err, user) => {
+      const contact = user.contacts.id(req.params.id);
+      if (err) {
+        res.send({'error' : 'An error has occurred'});
+      } else {
+        user.contacts.deleteOne(contact);
 
-    // Need implementation
+        user.save(err => {
+          if (err) res.status(400).send({error: err});
+          else res.send(contact + ' has been removed.');
+        });
+      }
+    });
   });
 
   //Searches by ID and updates contact with the new info
   app.put('/contacts/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
+    User.findOne({ email: email}, (err, user) => {
+      const contact = user.contacts.id(req.params.id);
+      if (err) {
+        res.send({'error' : 'An error has occurred'});
+      } else {
+        user.contacts.update(contact, req.body);
+        var newContact = user.contacts[0];
 
-    // Need implementation
+        user.save(err => {
+          if (err) res.status(400).send({error: err});
+          else res.send(newContact)
+        });
+      }
+    });
   });
 
   //Inserts new document into collection
@@ -59,6 +78,6 @@ module.exports = function(app, dbase) {
         });
       }
     });
-  }); 
-  
+  });
+
 };
