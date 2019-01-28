3e37372ca37f8a77b519f1f0327973225bd0f13e
SteffeyDev <steffeydev@icloud.com>
2019-01-28 10:29:28 -0500

    Moving app/routes to routes

--- back-end/app/routes/index.js
+++
@@ -1,7 +0,0 @@
-const contactRoutes = require('./contact_routes');
-const authRoutes = require('./auth_routes');
-
-module.exports = function(app, db){
-  contactRoutes(app, db);
-  authRoutes(app, db);
-};
