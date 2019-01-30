78b1cbd58f621cfba86f3e54a9fc205c4e4d356d
SteffeyDev <steffeydev@icloud.com>
2019-01-30 10:33:53 -0500

    Switching from webfonts fontawesome to js

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <!--FontAwesome stylesheet reference-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-          crossorigin="anonymous">
     <Home v-if="authenticated" />
     <Login v-else />
   </div>
