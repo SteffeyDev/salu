ec213b1f462b5479db5038b05812f645b178976c
mibeh18@gmail.com <root@salu.pro>
2019-02-01 19:08:53 +0000

    Fixing scripts

--- scripts/uploadManyContacts.js
+++ scripts/uploadManyContacts.js
@@ -1,15 +1,8 @@
 //email: DemoUser@salu.pro
 //username: DemoUser
 // password: salupro
-const contacts = require("MOCK_DATA.json");
 conn = new Mongo();
 db = conn.getDB("exampleDb");
-db.people.findOne({ username : "DemoUser"}, (err, user) => {
-        if (err || !user) {
-            print('An error has occured');
-        } else {
-            contacts.forEach(contact => user.contacts.unshift(contacts));
-            user.save();
-        }
-    })
-});
+let sample_user = db.people.findOne({ username : "DemoUser"});
+sample_contacts.forEach(contact => sample_user.contacts.push(contact));
+db.people.save(sample_user);
