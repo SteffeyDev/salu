1dff4154a2e79693173eba21c784a06d83346c08
SteffeyDev <steffeydev@icloud.com>
2019-02-02 15:16:54 -0500

    Script change take 2

--- scripts/uploadManyContacts.js
+++ scripts/uploadManyContacts.js
@@ -5,7 +5,7 @@ conn = new Mongo();
 db = conn.getDB("exampleDb");
 let tags = ['UCF', 'Work', 'Meetup', 'Conference'];
 let sample_user = db.people.findOne({ username : "DemoUser"});
-sample_user.contacts.forEach(c => c.remove());
+sample_user.contacts = [];
 sample_contacts.forEach(contact => {
   let tag = Math.random() >= 0.4 ? tags[Math.floor(Math.random() * tags.length)] : null;
   sample_user.contacts.push(Object.assign({ _id: ObjectId() }, contact));
