fd32e6e70a5fd9337bc526d064c1959b004cba59
SteffeyDev <steffeydev@icloud.com>
2019-02-02 16:13:23 -0500

    Adding tags to sample data script

--- scripts/uploadManyContacts.js
+++ scripts/uploadManyContacts.js
@@ -7,7 +7,9 @@ let tags = ['UCF', 'Work', 'Meetup', 'Conference'];
 let sample_user = db.people.findOne({ username : "DemoUser"});
 sample_user.contacts = [];
 sample_contacts.forEach(contact => {
-  let tag = Math.random() >= 0.4 ? tags[Math.floor(Math.random() * tags.length)] : null;
-  sample_user.contacts.push(Object.assign({ _id: ObjectId() }, contact));
+  let user_tags = [];
+  Math.random() >= 0.4 && user_tags.push(tags[Math.floor(Math.random() * tags.length)]);
+  Math.random() >= 0.4 && user_tags.push(tags[Math.floor(Math.random() * tags.length)]);
+  sample_user.contacts.push(Object.assign({ _id: ObjectId(), tags: user_tags }, contact));
 });
 db.people.save(sample_user);
